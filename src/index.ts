df8c3769dff1549d0cd92ecad4fae16f86306209
mackie <preciousmbaekwe@gmail.com>
2024-11-02 09:07:07 +0100

    feat: add basic landing page for API with documentation link

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,17 @@ import walletRoutes from "@/routes/walletRoutes";
 import transactionRoutes from "@/routes/transactionRoutes";
 import bodyParser from "body-parser";
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use("/api/users", userRoutes);
 
